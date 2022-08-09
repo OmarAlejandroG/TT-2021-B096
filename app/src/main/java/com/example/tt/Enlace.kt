87f4800f188ffe0b346a3ec427c57d73ace37e79
Omar <matsukazenutty@gmail.com>
2022-08-09 00:27:47 -0500

    Proyecto V0.3 -BlueTooth implementado -Se despliegan los dispositivos BT vinculados -Intenta la conexión

--- app/src/main/java/com/example/tt/Enlace.kt
+++ app/src/main/java/com/example/tt/Enlace.kt
@@ -1,20 +1,88 @@
 package com.example.tt
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
+import android.util.Log
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_enlace.*
+import com.ingenieriajhr.blujhr.BluJhr
 
 class Enlace : AppCompatActivity() {
+
+    lateinit var blue: BluJhr
+    var devicesBluetooth = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enlace)
 
-        val ref: ImageButton = findViewById(R.id.ref)
+        blue = BluJhr(this)
+        blue.onBluetooth()
+
+        bt_list.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(applicationContext, "Conexión", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, menu_principal::class.java)
+            intent.putExtra("addres", devicesBluetooth[i])
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (blue.checkPermissions(requestCode,grantResults)){
+            Toast.makeText(this, "Encendido de BlueTooth", Toast.LENGTH_SHORT).show()
+            blue.initializeBluetooth()
+        }else{
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.S){
+                blue.initializeBluetooth()
+            }else{
+                Toast.makeText(this, "Algo salió mal", Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (!blue.stateBluetoooth() && requestCode == 100){
+            blue.initializeBluetooth()
+        }else{
+            if (requestCode == 100){
+                devicesBluetooth = blue.deviceBluetooth()
+                if (devicesBluetooth.isNotEmpty()){
+                    val adapter = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,devicesBluetooth)
+                    bt_list.adapter = adapter
+                }else{
+                    Toast.makeText(this, "No tienes dispositivos vinculados", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+}
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enlace)
+
+        /*val ref: ImageButton = findViewById(R.id.ref)
         ref.setOnClickListener{
             startActivity(Intent(this, menu_principal::class.java))
         }
 
-    }
-}
+        val bt_list : ListView = findViewById(R.id.bt_list)*/
+
+
+
+    }*/
--- app/src/main/java/com/example/tt/menu_principal.kt
+++ app/src/main/java/com/example/tt/menu_principal.kt
@@ -6,16 +6,56 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
+import com.ingenieriajhr.blujhr.BluJhr
 
 class menu_principal : AppCompatActivity() {
+
+    var addres = ""
+
+    lateinit var blu: BluJhr
+
+    var estadoConexion = BluJhr.Connected.False
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
 
-        val btn_back: ImageButton = findViewById(R.id.btn_back)
-        btn_back.setOnClickListener{
-            startActivity(Intent(this, Enlace::class.java))
-        }
+        addres = intent.getStringExtra("addres").toString()
+
+        blu = BluJhr(this)
+
+        blu.setDataLoadFinishedListener(object:BluJhr.ConnectedBluetooth{
+            override fun onConnectState(state: BluJhr.Connected) {
+                when (state) {
+                    BluJhr.Connected.True -> {
+                        Toast.makeText(applicationContext, "Conexión establecida", Toast.LENGTH_SHORT).show()
+                        estadoConexion = state
+                        rxReceived()
+                    }
+                    BluJhr.Connected.Pending -> {
+                        Toast.makeText(applicationContext, "Conexión pendiente", Toast.LENGTH_SHORT).show()
+                        estadoConexion = state
+
+                    }
+                    BluJhr.Connected.False -> {
+                        Toast.makeText(applicationContext, "Conexión fallida", Toast.LENGTH_SHORT).show()
+                        estadoConexion = state
+                        blu.closeConnection()
+                        true
+                        startActivity(Intent(applicationContext, Enlace::class.java))
+                    }
+                    BluJhr.Connected.Disconnect -> {
+                        Toast.makeText(applicationContext, "Desconectar", Toast.LENGTH_SHORT).show()
+                        estadoConexion = state
+                        startActivity(Intent(applicationContext,MainActivity::class.java))
+                    }
+                }
+            }
+        })
+
+        /*btnSend.setOnClickListener {
+            blu.bluTx(edtSend.text.toString())
+        }*/
 
         val ad_mp: Button = findViewById(R.id.ad_mp)
         ad_mp.setOnClickListener{
@@ -32,7 +72,7 @@ class menu_principal : AppCompatActivity() {
             Toast.makeText(applicationContext, btn_batt.text, Toast.LENGTH_SHORT).show()
         }
 
-        val btn_cm: Button = findViewById(R.id.btn_cm)
+        /*val btn_cm: Button = findViewById(R.id.btn_cm)
         btn_cm.setOnClickListener{
             startActivity(Intent(this, control_manual::class.java))
         }
@@ -40,7 +80,28 @@ class menu_principal : AppCompatActivity() {
         val btn_opc: Button = findViewById(R.id.btn_opc)
         btn_opc.setOnClickListener{
             startActivity(Intent(this, Opciones::class.java))
+        }*/
+
+        val btn_back: ImageButton = findViewById(R.id.btn_back)
+        btn_back.setOnClickListener{
+            blu.closeConnection()
+            true
+            startActivity(Intent(this, Enlace::class.java))
+        }
+
+        if (estadoConexion != BluJhr.Connected.True){
+            blu.connect(addres)
         }
 
     }
+
+    private fun rxReceived() {
+        blu.loadDateRx(object:BluJhr.ReceivedData{
+            override fun rxDate(rx: String) {
+                //txtConsola.text = txtConsola.text.toString() +rx
+                Toast.makeText(this@menu_principal, "Recibido", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }
